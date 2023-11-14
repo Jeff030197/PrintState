589c1c4cbb9f0e5fd5f10867c73be5073931528f
Sim Lloyd <separessimlloyd.com>
2023-11-14 23:39:18 +0800

    add cart icon

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import logo from "../img/logo.jpg"
 function Header() {
   return (
     <>
-      <div className="bg-pink-600 h-18">
+      <div className="bg-white h-18">
       <nav>
         <div className="grid grid-cols-3 py-2">
         <span className="pl-5 flex flex-col-2">
@@ -18,6 +18,11 @@ function Header() {
             <a href="" className="px-5">CONTACT</a>
           </div>
           <div className="flex justify-center py-4 uppercase">
+            <div>
+            <svg class="w-6 h-6 text-gray-800 dark:text-pink-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 19 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm1-4H5m0 0L3 4m0 0h5.501M3 4l-.792-3H1m11 3h6m-3 3V1"/>
+            </svg>
+            </div>
             <div className="px-4">
             <a href="" className="px-4 py-2 bg-pink-400 rounded-md">login</a>
             </div>
